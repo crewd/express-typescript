bb406861032ed0b68dd9c7d2737af4f471afc369
crewd <newspaper321@nate.com>
2022-12-19 17:41:59 +0900

    post entity 추가

---
+++ src/Post/post.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  wirter: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
